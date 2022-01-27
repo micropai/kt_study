bf8f58741fbae28c1a3cd5868600d316f36185e9
micro <test>
2022-01-27 14:00:24 +0900

    [prop] 프로퍼티 사용법 주석 기술

--- src/main/java/com/example/springbasic/configuration/BasicCustomProp.java
+++ src/main/java/com/example/springbasic/configuration/BasicCustomProp.java
@@ -5,19 +5,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * 기본적인 프로퍼티 바인딩 예시
+ */
 @Configuration
 @Getter
 public class BasicCustomProp {
+    /**
+     * 기본적인 value 어노테이션을 통한 바인딩
+     */
     @Value("${custom.basic.name}")
     private String name;
+    
     private String url;
     private final String type;
 
+    /**
+     * 생성자를 통한 프로퍼티 바인딩
+     * @param type
+     */
     BasicCustomProp(@Value("${custom.basic.type}") String type){
         this.type = type;
     }
 
-
+    /**
+     * autowired 를 통한 프로퍼티 바인딩
+     */
     @Autowired
     public void setProp(@Value("${custom.basic.url}")String url){
         this.url = url;
--- src/main/java/com/example/springbasic/configuration/ConfigCustomProp.java
+++ src/main/java/com/example/springbasic/configuration/ConfigCustomProp.java
@@ -4,6 +4,11 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+/**
+ * ConfigurationProperties 를 통한 프로퍼티 바인딩
+ * yml 로 여러 키를 가진 list object 로도 바인딩됩니다.
+ * 최신 프로퍼티에서는 yml 이 아니고 properties 파일도 가능함 추후 예시추가
+ */
 @ConfigurationProperties("custom.configuration")
 @Getter
 @Setter
