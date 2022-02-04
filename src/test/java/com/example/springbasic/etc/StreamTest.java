8ab44e2d85ad88ec76ee0a3387ec6af1152a8115
micro <micropai@naver.com>
2022-02-04 18:02:23 +0900

    [stream] 정규식 stream test code 추가

--- src/test/java/com/example/springbasic/etc/StreamTest.java
+++ src/test/java/com/example/springbasic/etc/StreamTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import java.util.Collections;
 import java.util.List;
 import java.util.PriorityQueue;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -32,8 +34,9 @@ public class StreamTest {
     @DisplayName("[stream] 특정값 또는 null 포함 등의 기본테스트")
     public void testOfStream(){
         // given
+        Stream<String> actual = Stream.of("a", "b", "c", null);
         // when & then
-        then(Stream.of("a", "b", "c", null))
+        then(actual)
                 .hasSize(4)
                 .containsNull()
                 .contains("a", "b")
@@ -43,16 +46,52 @@ public class StreamTest {
     }
 
     @Test
+    @DisplayName("[stream] stream 생성 add 와 of")
+    public void testOfAdd(){
+        // given
+        Stream<Integer> actual = Stream.<Integer>builder().add(1).add(2).add(3).build();
+        Stream<Integer> excepted = Stream.of(1, 2, 3);
+
+        // when & then
+        then(actual)
+                .hasSize(3)
+                .containsSequence(excepted.collect(Collectors.toList()));
+    }
+    
+    @Test
+    @DisplayName("[stream] stream generate 생성 ")
+    public void testGenerateStream(){
+        // given
+        Stream<String> actual = Stream.generate(() -> "A").limit(5);
+        // when & then
+        then(actual)
+                .hasSize(5)
+                .containsExactly("A","A","A","A","A");
+    
+    }
+
+    @Test
     @DisplayName("[stream] iterate 생성 테스트")
     public void testIterateStream(){
         // given
-
+        Stream<Integer> actual = Stream.iterate(2, i-> i < 10, i-> i+2);
         // when & then
-        then(Stream.iterate(2, i-> i < 10, i-> i+2))
+        then(actual)
                 .hasSize(4)
                 .doesNotContain(0, 10)
                 .contains(6, 8)
                 .containsExactly(2, 4, 6, 8);
+    }
+
+    @Test
+    @DisplayName("[stream] 정규식 stream 생성")
+    public void testRexStream(){
+        // given
+        Stream<String> actual = Pattern.compile(",").splitAsStream("a,b,c");
+        // when & then
+        then(actual)
+                .hasSize(3)
+                .containsExactly("a", "b", "c");
     }
 
     @Test
@@ -87,16 +126,14 @@ public class StreamTest {
         List<Integer> list = List.of(1, 3, 3, 2, 1, 2);
 
         PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
-        list.stream().forEach(priorityQueue::add);
+        priorityQueue.addAll(list);
 
         // when
         // IntStream.of(1, 2, 3, 1, 2, 3).boxed().sorted(Collections.reverseOrder())
         Stream<Integer> actual = list.stream().sorted(Collections.reverseOrder());
 
         // then
-        then(actual).allSatisfy(i -> {
-            then(i).isEqualTo(priorityQueue.poll());
-        });
+        then(actual).allSatisfy(i -> then(i).isEqualTo(priorityQueue.poll()));
         // stream 이 닫혔기 때문에 실패나야 합니다.~
         thenThrownBy(()->then(actual).containsSequence(priorityQueue));
     }
