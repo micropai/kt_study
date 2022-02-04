641f0b542be6080dcb71cd84dc1dd01bf83afa4b
micro <micropai@naver.com>
2022-02-04 11:26:20 +0900

    [lambda] fix #5 - lambda test code 추가

--- src/test/java/com/example/springbasic/etc/LambdaTest.java
+++ src/test/java/com/example/springbasic/etc/LambdaTest.java
@@ -1,4 +1,23 @@
 package com.example.springbasic.etc;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.function.Predicate;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
 public class LambdaTest {
+    @Test
+    @DisplayName("[lambda] Predicate test code")
+    public void test(){
+        // given
+
+        // when
+        Predicate<Integer> isEven = i -> i%2==0;
+        // then
+        then(isEven.test(3)).isFalse();
+        then(isEven.test(8)).isTrue();
+
+    }
 }
